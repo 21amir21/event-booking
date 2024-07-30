b591cb55d25c0d9f93660230de1b360fc565022a
Amir Osama <amirosama21@gmail.com>
2024-07-30 20:11:00 +0300

    chore: Remove unnecessary line in HashPassword function

--- utils/hash.go
+++ utils/hash.go
@@ -4,7 +4,6 @@ import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-
 	return string(bytes), err
 }
 
